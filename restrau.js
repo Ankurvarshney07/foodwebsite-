8e553ec93079ea5a078ed65051a21fec5fcb4b8c
Ankurvarshney07 <ankur.varshney_cs19@gla.ac.in>
2021-09-26 11:43:23 +0530

    loader

--- restrau.js
+++ restrau.js
@@ -76,13 +76,3 @@ var swiper = new Swiper(".home-slider", {
    },
   });
 
-function loader(){
-  document.querySelector('.loader-container').classList.add('fade-out');
-
-}  
-
-function fadeOut(){
-  setInterval(loader, 0);
-}
-
-window.onload = fadeOut;
